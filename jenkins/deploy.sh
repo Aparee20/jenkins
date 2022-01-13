b3d2f024db3cbd36c636c151368083d03c8e7d35
ankurpareek <ankur.pareek20@gmail.com>
2022-01-13 13:57:03 +1100

    test

---
+++ jenkins/deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+echo "Packaging Build version >>>> "$build_version
---
+++ jenkins/package.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+
+source ../version.config
+build_version:$build_version
+
+
+
+echo "Packaging Build version >>>> "$build_version
+
+branch_name=$(git symbolic-ref -q HEAD)
+branch_name=${branch_name##refs/heads/}
+branch_name=${branch_name:-HEAD}
+
+
+echo "The branch to package is  >>>> $branch_name and the version is >>>> $build_version "
+
+package_name="$branch_name-$build_version"
+echo "package_name"
+
+
+##### Zip the files
+zip -r ../$package_name.zip ../src
+
+
+
